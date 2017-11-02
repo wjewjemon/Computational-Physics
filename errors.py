5f54b75603afe7b55ebc9c2553c70c6a4adf978c
Ben Cook <benjam@localhost.localdomain>
2017-11-02 18:00:20 -0400

    checked relative error

--- errors.py
+++ errors.py
@@ -51,3 +51,26 @@ approx2 = two_pass(data)
 
 print("1:", relative_error(actual, approx1))
 print("2:", relative_error(actual, approx2))
+
+''' Now lets explore the std of a sequence with predetermined variance. '''
+from numpy.random import normal
+seq1 = normal(0., 1., 2000)
+seq2 = normal(1.e7, 1., 2000)
+
+sig1p1 = one_pass(seq1)
+sig2p1 = two_pass(seq1)
+ac1 = std(seq1, ddof=1)
+rel1p1 = relative_error(ac1, sig1p1)
+rel2p1 = relative_error(ac1, sig2p1)
+
+sig1p2 = one_pass(seq2)
+sig2p2 = two_pass(seq2)
+ac2 = std(seq2, ddof=1)
+rel1p2 = relative_error(ac2, sig1p2)
+rel2p2 = relative_error(ac2, sig2p2)
+
+print("Relative Errors:")
+print("seq1, one_pass:", rel1p1)
+print("seq1, two_pass:", rel2p1)
+print("seq1, one_pass:", rel1p2)
+print("seq1, two_pass:", rel2p2)
